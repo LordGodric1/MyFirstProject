e06796a08fe125b7463bf9c26b92c11258e7e64b
tbeau <tbeau@WINTERS-BLOOD>
2022-01-30 17:43:54 -0500

    Add project files.

---
+++ MyFirstPractice/Program.cs
@@ -0,0 +1,6 @@
+﻿
+Console.WriteLine("Practice Line 1: lets add a line or 2 of code");
+Console.WriteLine("Practice Line 2: Please add text or enter for none to complete program");
+    Console.ReadLine();
+Console.Write("You did not add line to Write so it did not enter down.");
+Console.WriteLine("I understand the Console Write vs WriteLine  and also Readline");
---
+++ Net6IsMuchCleaner/Program.cs
@@ -0,0 +1,5 @@
+﻿
+Console.WriteLine("Using Net6 is much cleaner");
+Console.WriteLine("How Do you like it so far");
+Console.WriteLine("don't forget to set as start up project when testing or it won't run this item");
+
